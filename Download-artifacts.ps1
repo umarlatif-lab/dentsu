d05c15c2efbe8329b014246b58e4d3836dc6f8ab
umarlatif-lab <78913169+umarlatif-lab@users.noreply.github.com>
2021-07-26 17:43:53 +0100

    Add files via upload

--- Download-artifacts.ps1
+++ Download-artifacts.ps1
@@ -1,6 +1,6 @@
 write-host 'AIB Customization: Downloading Artifacts'
 $LocalPath = 'C:\\buildArtifacts'
-$buildArtifacts="https://euw1wvdgermany.file.core.windows.net/dmlapps/AIB_DML_BuildArtifacts.zip?sv=2020-04-08&ss=btqf&srt=sco&st=2021-07-12T13%3A31%3A27Z&se=2021-07-13T13%3A31%3A27Z&sp=rl&sig=U1zz0zc5zmm9QzJTMkD1nHurmASIiMtup9m%2FWYb8%2B8k%3D"
+$buildArtifacts="https://euw1wvdgermany.file.core.windows.net/dmlapps/AIB_DML_BuildArtifacts.zip?sv=2020-04-08&ss=btqf&srt=sco&st=2021-07-26T16%3A22%3A08Z&se=2021-07-27T16%3A22%3A08Z&sp=rl&sig=A8O0ZdU43MsaWxLa08qBavD8DKSP2qwPmY1gJikQlTg%3D"
 $installerFile="AIB_DML_BuildArtifacts.zip"
 
 Invoke-WebRequest $buildArtifacts -OutFile $LocalPath\$installerFile
