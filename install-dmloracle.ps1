14b814d45d34d4a07a1db80d74c617fbc911ee40
umarlatif-lab <78913169+umarlatif-lab@users.noreply.github.com>
2021-07-30 11:56:57 +0100

    Add files via upload

---
+++ install-dmloracle.ps1
@@ -0,0 +1,2 @@
+cd "C:\buildArtifacts\AIB_DML_BuildArtifacts\Oracle 64bit\Oracle 64bit"
+.\setup.exe -silent -nowait -responseFile "C:\buildArtifacts\AIB_DML_BuildArtifacts\client.rsp"
---
+++ install-oracleodac.ps1
@@ -0,0 +1,2 @@
+cd "C:\buildArtifacts\AIB_DML_BuildArtifacts\Oracle ODAC Client\Oracle ODAC Client\ODAC1120320_x64"
+.\setup.exe -silent -nowait -responseFile "C:\buildArtifacts\AIB_DML_BuildArtifacts\odacresponse.rsp"
